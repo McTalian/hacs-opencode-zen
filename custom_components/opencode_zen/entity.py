1f7eb988caa5c794d7bffa59b3315f783cb9408a
Rob Anderson <Mctalian@users.noreply.github.com>
2026-07-26 11:44:18 -0400

    chore: fix validation pre-release

--- custom_components/opencode_zen/entity.py
+++ custom_components/opencode_zen/entity.py
@@ -1,3 +1,19 @@
+# OpencodeZen - Home Assistant integration for OpenCode AI
+# Copyright (C) 2026 Rob "McTalian" Anderson
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 """Base entity for OpencodeZen."""
 
 import base64
@@ -209,24 +225,7 @@ async def async_prepare_files_for_prompt(
 
 
 class OpencodeZenEntity(Entity):
-# OpencodeZen - Home Assistant integration for OpenCode AI
-# Copyright (C) 2026 Rob "McTalian" Anderson
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-"""Base entity for OpencodeZen."""
-
+    """Base entity for OpencodeZen."""
     _attr_has_entity_name = True
 
     def __init__(self, entry: OpencodeZenConfigEntry, subentry: ConfigSubentry) -> None:
